c39c5007c724cc93e5d0ea54001b51f3d76ffbf9
Jon M <j.m4k119@gmail.com>
2023-02-03 02:44:02 -0500

    javascript assignment
    final part need help

--- Starter Code Challenge HTML & CSS 4/starter-code/javascript_fundamentals.js
+++ Starter Code Challenge HTML & CSS 4/starter-code/javascript_fundamentals.js
@@ -13,7 +13,8 @@ Using the variables above, add these variables together and multiply by 3.
 Assign the expression to a variable called "result" and console.log its value. 
 */
 
-(num1 + num2) * 3
+let result = (num1 + num2) * 3
+console.log(result);
 
 /*
 Step 3
@@ -21,22 +22,30 @@ Using the result variable, write an if/else statement comparing the result to th
 If the result is larger than 50, console.log "Result is bigger". 
 If it is smaller, console.log "Result is smaller"
 */
-
+if (result > 50) {
+  console.log("Result is bigger");
+}
+else if (result < 50) {
+  console.log("Result is smaller");
+}
 
 /*
 Arrays & Loops
 Step 1
 Create an empty array and assign it to a variable called "numberList".
 */
-
+let numberList = [];
 /*
 Step 2
 Using a for loop, place the numbers from 0 to 5 into the "numberList" array.
 console.log the numberList array.
 */
-
+for (let i = 0; i < 6;  i++) {
+  console.log(numberList[i])
+}
 /*
 Step 3
 Remove the last number in the array and console.log the array.
 */
-
+numberList.pop();
+console.log(numberList);
